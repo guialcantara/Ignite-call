432a33d89bf86ece8b099033813fe4d92a0f0774
Guilherme Alcântara <gui.absilva@outlook.com>
2022-12-28 16:57:50 -0300

    add: redirecionamento do usuário para pagina register

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,6 +1,8 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { Container, Form, FormError, Header } from './styles'
@@ -13,16 +15,26 @@ const registerFormSchema = z.object({
     .transform((username) => username.toLowerCase()),
   name: z.string().min(3, { message: 'Digite no minimo 3 letras' }),
 })
+
 type RegisterFormData = z.infer<typeof registerFormSchema>
 
 export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
   })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
     console.log(data)

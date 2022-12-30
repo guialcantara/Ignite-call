d3b1a6e12c2b7884a9ffe257091d8fd3ed57a592
Guilherme Alcântara <gui.absilva@outlook.com>
2022-12-30 10:44:32 -0300

    Add: Prisma, Axios, rota de cadastro de usuário

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -5,8 +5,9 @@ import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { api } from '../../lib/axios'
 import { Container, Form, FormError, Header } from './styles'
-
+import { AxiosError } from 'axios'
 const registerFormSchema = z.object({
   username: z
     .string()
@@ -38,6 +39,18 @@ export default function Register() {
 
   async function handleRegister(data: RegisterFormData) {
     console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+      console.log(err)
+    }
   }
   return (
     <Container>
